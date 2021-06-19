85688495891d1a876ad2a0a33bcdf0dd9d24ba22
BurmistrovaEO <tigrettokat@mail.ru>
2021-06-19 12:26:09 +0300

    Recognition v.1.0

--- android-client/Hackaton/app/src/main/java/com/example/hackatonapp/presentation/screen/camera/CameraFragment.kt
+++ android-client/Hackaton/app/src/main/java/com/example/hackatonapp/presentation/screen/camera/CameraFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hackatonapp.presentation.screen.camera
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,6 +9,7 @@ import androidx.fragment.app.viewModels
 import com.example.hackatonapp.R
 import com.example.hackatonapp.databinding.FragmentCameraBinding
 import com.example.hackatonapp.presentation.extensions.viewBinding
+
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
 
@@ -16,6 +19,7 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
+
+
 }
