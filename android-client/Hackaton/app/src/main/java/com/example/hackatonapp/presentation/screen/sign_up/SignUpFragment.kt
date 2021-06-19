03d001d0dad99160353ae8962f80a111e24d7556
Ilya Gushchin <bulldoo@yandex.ru>
2021-06-19 16:37:27 +0300

    Important refactor

--- android-client/Hackaton/app/src/main/java/com/example/hackatonapp/presentation/screen/sign_up/SignUpFragment.kt
+++ android-client/Hackaton/app/src/main/java/com/example/hackatonapp/presentation/screen/sign_up/SignUpFragment.kt
@@ -12,6 +12,7 @@ import com.example.hackatonapp.R
 import com.example.hackatonapp.data.entities.User
 import com.example.hackatonapp.databinding.FragmentRegistrationBinding
 import com.example.hackatonapp.presentation.extensions.viewBinding
+import com.example.hackatonapp.utils.Resource
 
 class SignUpFragment : Fragment(R.layout.fragment_registration) {
 
@@ -31,9 +32,9 @@ class SignUpFragment : Fragment(R.layout.fragment_registration) {
 
     private fun setListeners() {
         binding.buttonStartRegistration.setOnClickListener {
-            val login = binding.etLoginRegistration.text.toString()
-            val password = binding.etPasswordRegistration.text.toString()
-            val snils = binding.etSnilsRegistration.text.toString()
+            val login = binding.loginSignUpTextEdit.text.toString()
+            val password = binding.passwordSignUpTextEdit.text.toString()
+            val snils = binding.snilsSignUpTextEdit.text.toString()
             val user = User(login, password, "pat", snils)
             if (password.isNotEmpty() && login.isNotEmpty() && snils.isNotEmpty()) {
                 if (password.length >= 6) {
@@ -62,12 +63,33 @@ class SignUpFragment : Fragment(R.layout.fragment_registration) {
         viewModel.userToken.observe(
             viewLifecycleOwner,
             Observer { response ->
-                binding.etSnilsRegistration.visibility = View.INVISIBLE
-                findNavController()
-                    .navigate(R.id.action_registrationFragment_to_patientDataListFragment)
-                saveToSharedPreferences(response)
+                when(response){
+                    is Resource.Success -> {
+                        hideProgressBar()
+                        response.data?.let { response ->
+                            saveToSharedPreferences(response)
+                        }
+                        findNavController()
+                            .navigate(R.id.action_registrationFragment_to_patientDataListFragment)
+                    }
+                    is Resource.Error -> {
+                        hideProgressBar()
+                        Toast.makeText(context, "Что-то пошло не так", Toast.LENGTH_SHORT).show()
+                    }
+                    is Resource.Loading -> {
+                        showProgressBar()
+                    }
+                }
             }
         )
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBarRegistration.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBarRegistration.visibility = View.VISIBLE
     }
 
     private fun saveToSharedPreferences(token: String){
